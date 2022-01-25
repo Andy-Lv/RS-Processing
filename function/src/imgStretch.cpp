0b5e6911e803fb445690c0262a35dd4e497fe422
andy <3304086606@qq.com>
2022-01-25 21:23:53 +0800

    stretch

--- function/src/imgStretch.cpp
+++ function/src/imgStretch.cpp
@@ -10,6 +10,7 @@ ImgStretch::ImgStretch(QWidget *parent)
 {
     ui_imgStretch->setupUi(this);
 
+    //选择输入图片
     connect(ui_imgStretch->pushButton_openInputFolder, &QPushButton::released, this,
             [=]()
             {
@@ -18,19 +19,12 @@ ImgStretch::ImgStretch(QWidget *parent)
 
                 Init();
 
-//                //设置范围值
-//                this->minimum = theImage.GetMinimumValue()[bandNum] + 1;
-//                this->maximum = theImage.GetMaximumValue()[bandNum] - 1;
-
-                //设置被拉伸像素值选择范围  初始值
-                ui_imgStretch->spinBox_preMinGrayscale->setMinimum(theImage.GetMinimumValue()[bandNum]);
-                ui_imgStretch->spinBox_preMaxGrayscale->setMaximum(theImage.GetMaximumValue()[bandNum]);
-
                 ui_imgStretch->spinBox_preMinGrayscale->setValue(theImage.GetMinimumValue()[bandNum]);
                 ui_imgStretch->spinBox_preMaxGrayscale->setValue(theImage.GetMaximumValue()[bandNum]);
 
             });
 
+    //选择输出图片路径
     connect(ui_imgStretch->pushButton_openOutputFolder, &QPushButton::released, this,
             [=]()
             {
@@ -38,6 +32,7 @@ ImgStretch::ImgStretch(QWidget *parent)
                         QFileDialog::getExistingDirectory(this, "pushButton_openOutputFolder"));
             });
 
+    //根据选择参数进行图像拉伸
     connect(ui_imgStretch->pushButton_ok, &QPushButton::released, this,
             [=]()
             {
@@ -69,6 +64,7 @@ ImgStretch::ImgStretch(QWidget *parent)
                 }
             });
 
+    //取消
     connect(ui_imgStretch->pushButton_cancel, &QPushButton::released, this, &QDialog::close);
 
     //上一个波段按钮
@@ -77,6 +73,8 @@ ImgStretch::ImgStretch(QWidget *parent)
             {
                 this->bandNum++;
                 this->Init();
+                ui_imgStretch->spinBox_preMinGrayscale->setValue(theImage.GetMinimumValue()[bandNum]);
+                ui_imgStretch->spinBox_preMaxGrayscale->setValue(theImage.GetMaximumValue()[bandNum]);
             });
     //下一个波段按钮
     connect(ui_imgStretch->pushButton_previous, &QPushButton::released, this,
@@ -84,8 +82,11 @@ ImgStretch::ImgStretch(QWidget *parent)
             {
                 this->bandNum--;
                 this->Init();
+                ui_imgStretch->spinBox_preMinGrayscale->setValue(theImage.GetMinimumValue()[bandNum]);
+                ui_imgStretch->spinBox_preMaxGrayscale->setValue(theImage.GetMaximumValue()[bandNum]);
             });
 
+    //拉伸灰度范围
     connect(ui_imgStretch->spinBox_preMinGrayscale, &QSpinBox::textChanged, this,
             [=]()
             {
@@ -97,7 +98,7 @@ ImgStretch::ImgStretch(QWidget *parent)
                 this->maximum = ui_imgStretch->spinBox_preMaxGrayscale->value();
                 this->Init();
             });
-
+    //拉伸灰度范围
     connect(ui_imgStretch->spinBox_preMaxGrayscale, &QSpinBox::textChanged, this,
             [=]()
             {
@@ -109,11 +110,44 @@ ImgStretch::ImgStretch(QWidget *parent)
                 this->maximum = ui_imgStretch->spinBox_preMaxGrayscale->value();
                 this->Init();
             });
+
+    //拉伸方法选择
+    connect(ui_imgStretch->radioButton_tif_default, &QRadioButton::toggled, this,
+            [=]()
+            {
+                this->ui_imgStretch->spinBox_laterMinGrayscale->setDisabled(true);
+                this->ui_imgStretch->spinBox_laterMaxGrayscale->setDisabled(true);
+                this->ui_imgStretch->radioButton_tif->setChecked(true);
+                this->ui_imgStretch->radioButton_tif_default->setChecked(true);
+                //this->ui_imgStretch->radioButton_png->setChecked(false);
+            });
+    connect(ui_imgStretch->radioButton_tif, &QRadioButton::toggled, this,
+            [=]()
+            {
+                this->ui_imgStretch->spinBox_laterMinGrayscale->setEnabled(true);
+                this->ui_imgStretch->spinBox_laterMaxGrayscale->setEnabled(true);
+                this->ui_imgStretch->radioButton_tif->setChecked(true);
+                this->ui_imgStretch->radioButton_tif_default->setChecked(false);
+                this->ui_imgStretch->radioButton_png->setChecked(false);
+            });
+    connect(ui_imgStretch->radioButton_png, &QRadioButton::toggled, this,
+            [=]()
+            {
+                this->ui_imgStretch->spinBox_laterMinGrayscale->setEnabled(true);
+                this->ui_imgStretch->spinBox_laterMaxGrayscale->setEnabled(true);
+                this->ui_imgStretch->radioButton_tif->setChecked(false);
+                this->ui_imgStretch->radioButton_tif_default->setChecked(false);
+                this->ui_imgStretch->radioButton_png->setChecked(false);
+            });
 }
 
 void ImgStretch::Init()
 {
+    this->InputImagePath = ui_imgStretch->lineEdit_inputPath->text();
+    theImage.ReadImage(InputImagePath.toStdString().c_str());
+
     this->bandNum_all = theImage.GetBandNum();
+
     //设置波段切换按钮
     ui_imgStretch->pushButton_previous->setEnabled(true);
     ui_imgStretch->pushButton_next->setEnabled(true);
@@ -126,11 +160,6 @@ void ImgStretch::Init()
         ui_imgStretch->pushButton_next->setDisabled(true);
     }
 
-
-    this->InputImagePath = ui_imgStretch->lineEdit_inputPath->text();
-
-    theImage.ReadImage(InputImagePath.toStdString().c_str());
-
     //设置顶端显示
     ui_imgStretch->label_bandNum->setText(QString::number(bandNum));
     ui_imgStretch->label_bandNum_all->setText(QString::number(theImage.GetBandNum()));
@@ -139,6 +168,10 @@ void ImgStretch::Init()
     this->ui_imgStretch->widget_histogram->DrawHistogram(theImage.GetHistogramArray()[bandNum],
                                                          theImage.GetMaximumValue()[bandNum],
                                                          minimum, maximum);
+
+    //设置被拉伸像素值选择范围  初始值
+    ui_imgStretch->spinBox_preMinGrayscale->setMinimum(theImage.GetMinimumValue()[bandNum]);
+    ui_imgStretch->spinBox_preMaxGrayscale->setMaximum(theImage.GetMaximumValue()[bandNum]);
 }
 
 void ImgStretch::ImageStretching_png()
@@ -148,120 +181,118 @@ void ImgStretch::ImageStretching_png()
 
 void ImgStretch::ImageStretching_tif()
 {
+    int imgWidth=theImage.GetImgWidth();
+    int imgHeight=theImage.GetImgHeight();
 
+    this->OutputImageName=this->ui_imgStretch->lineEdit_outputName->text();
+    this->OutputImagePath=this->ui_imgStretch->lineEdit_outputPath->text();
+
+    GDALAllRegister();
+
+    //原始图像
+    GDALDataset *InputImage = (GDALDataset *) GDALOpen(InputImagePath.toStdString().c_str(), GA_ReadOnly);
+
+    //图像驱动
+    GDALDriver *poDriver = GetGDALDriverManager()->GetDriverByName("GTiff");
+
+    QString path = OutputImagePath + "/" + OutputImageName + "tif";
+    //创建16bit的数据
+    GDALDataset *OutputImage = poDriver->Create(path.toStdString().c_str(), imgWidth, imgHeight, bandNum,
+                                                GDT_UInt16,
+                                                NULL);
+    double dGeoTrans[6] = {0};
+
+    //设置仿射变换参数
+    InputImage->GetGeoTransform(dGeoTrans);
+    OutputImage->SetGeoTransform(dGeoTrans);
+    //设置图像投影信息
+    OutputImage->SetProjection(InputImage->GetProjectionRef());
+
+    //用于保存读取的16bit数据
+    GUInt16 *InputData = new GUInt16[imgHeight * imgWidth];
+    //用于输出的16bit数据
+    GUInt16 *OutputData = new GUInt16[imgHeight * imgWidth];
+
+    double minimumValue, maximumValue, meanValue, sigmaValue;
+
+    //定义直方图段数和数组
+    int histogramBuckets = maximumValue;
+    unsigned long long *histogramArray = new unsigned long long[histogramBuckets];
+
+    //循环波段
+    for (int iBand = 1; iBand <= bandNum; iBand++)
+    {
+        GDALRasterBand *InputBand = InputImage->GetRasterBand(iBand);
+        GDALRasterBand *OutputBand = OutputImage->GetRasterBand(iBand);
+
+        //获取该波段的最大值 最小值 平均值 和标准差
+        InputBand->GetStatistics(true, true, &minimumValue, &maximumValue, &meanValue, &sigmaValue);
+
+        //获取该波段灰度直方图
+        InputBand->GetHistogram(
+                minimumValue - std::numeric_limits<double>::epsilon(),
+                maximumValue + std::numeric_limits<double>::epsilon(),
+                histogramBuckets,
+                histogramArray,
+                false,
+                false,
+                GDALDummyProgress,
+                nullptr);
+
+        //定义线性拉伸界限
+        double minPercent = 0;
+        double maxPercent = 1;
+
+        unsigned long long p1 = 0;
+        unsigned long long p2 = histogramBuckets - 1;
+
+        while (minPercent < 0.02)
+        {
+            minPercent += (double) histogramArray[p1++] /
+                          double(imgWidth * imgHeight);
+        }
+        while (maxPercent > 0.98)
+        {
+            maxPercent -= (double) histogramArray[p2--] /
+                          double(imgWidth * imgHeight);
+        }
+
+        //确定线性拉伸范围灰度值界限
+        double gap = maximumValue - minimumValue;
+        double pixgap = gap / (double) histogramBuckets;
+        double minpix = p1 * pixgap + minimumValue;
+        double maxpix = p2 * pixgap + minimumValue;
+        double stretched_gap = maxpix - minpix;
+
+        //将数据读出来
+        InputBand->RasterIO(GF_Read, 0, 0, imgWidth, imgHeight, InputData, imgWidth, imgHeight, GDT_UInt16, 0, 0);
+
+        for (int i = 0; i < imgHeight; i++)    //循环图像高
+        {
+            //循环，将16bit数据专为8bit数据，使用线性拉伸方式
+            for (int j = 0; j < imgWidth; j++)
+            {
+                if (InputData[i * imgWidth + j] < minpix)
+                    OutputData[i * imgWidth + j] = 0;
+                else if (InputData[i * imgWidth + j] >= minpix && InputData[i * imgWidth + j] < maxpix)
+                {
+                    double temp = ((double) InputData[i * imgWidth + j] - minpix) / stretched_gap * gap;
+                    OutputData[i * imgWidth + j] = (GUInt16) temp;
+                    //cout<<OutputData[i * imgWidth + j]<<endl;
+                }
+                if (InputData[i * imgWidth + j] > maxpix)
+                    OutputData[i * imgWidth + j] = 255;
+            }
+        }
+        OutputBand->RasterIO(GF_Write, 0, 0, imgWidth, imgHeight, OutputData, imgWidth, imgHeight, GDT_UInt16, 0, 0);
+    }
+
+    delete[] InputData;
+    delete[] OutputData;
+    //关闭原始图像和结果图像
+    GDALClose((GDALDatasetH) OutputImage);
+    GDALClose((GDALDatasetH) InputImage);
 }
-//{
-//    //TODO 确定输入图像是多少位的，是否需要拉伸到不同的位，但感觉没什么意义
-//    GDALAllRegister();
-//
-//    //原始图像
-//    GDALDataset *InputImage = (GDALDataset *) GDALOpen(InputImagePath.toStdString().c_str(), GA_ReadOnly);
-//
-//    //图像驱动
-//    GDALDriver *poDriver = GetGDALDriverManager()->GetDriverByName("GTiff");
-//
-//    QString path = OutputImagePath + "/" + OutputImageName + "tif";
-//    //创建8bit的数据
-//    GDALDataset *OutputImage = poDriver->Create(path.toStdString().c_str(), imgWidth, imgHeight, bandNum,
-//                                                GDT_Byte,
-//                                                NULL);
-//    double dGeoTrans[6] = {0};
-//
-//    //设置仿射变换参数
-//    InputImage->GetGeoTransform(dGeoTrans);
-//    OutputImage->SetGeoTransform(dGeoTrans);
-//    //设置图像投影信息
-//    OutputImage->SetProjection(InputImage->GetProjectionRef());
-//
-//    //用于保存读取的16bit数据
-//    GUInt16 *InputData = new GUInt16[imgHeight * imgWidth];
-//    //用于输出的8bit数据
-//    GByte *OutputData = new GByte[imgHeight * imgWidth];
-//
-//    double minimumValue, maximumValue, meanValue, sigmaValue;
-//
-//    //定义直方图段数和数组
-//    int histogramBuckets = 1024;
-//    unsigned long long *histogramArray = new unsigned long long[histogramBuckets];
-//
-//    //循环波段
-//    for (int iBand = 1; iBand <= bandNum; iBand++)
-//    {
-//        GDALRasterBand *InputBand = InputImage->GetRasterBand(iBand);
-//        GDALRasterBand *OutputBand = OutputImage->GetRasterBand(iBand);
-//
-//        //获取该波段的最大值 最小值 平均值 和标准差
-//        InputBand->GetStatistics(true, true, &minimumValue, &maximumValue, &meanValue, &sigmaValue);
-//
-//        //获取该波段灰度直方图
-//        InputBand->GetHistogram(
-//                minimumValue - std::numeric_limits<double>::epsilon(),
-//                maximumValue + std::numeric_limits<double>::epsilon(),
-//                histogramBuckets,
-//                histogramArray,
-//                false,
-//                false,
-//                GDALDummyProgress,
-//                nullptr);
-//
-//        //定义线性拉伸界限
-//        double minPercent = 0;
-//        double maxPercent = 1;
-//
-//        unsigned long long p1 = 0;
-//        unsigned long long p2 = histogramBuckets - 1;
-//
-//        while (minPercent < 0.02)
-//        {
-//            minPercent += (double) histogramArray[p1++] /
-//                          double(imgWidth * imgHeight);
-//        }
-//        while (maxPercent > 0.98)
-//        {
-//            maxPercent -= (double) histogramArray[p2--] /
-//                          double(imgWidth * imgHeight);
-//        }
-//
-//        //确定线性拉伸范围灰度值界限
-//        double gap = maximumValue - minimumValue;
-//        double pixgap = gap / (double) histogramBuckets;
-//        double minpix = p1 * pixgap + minimumValue;
-//        double maxpix = p2 * pixgap + minimumValue;
-//        double stretched_gap = maxpix - minpix;
-//
-//
-//
-//
-//
-//        //将数据读出来
-//        InputBand->RasterIO(GF_Read, 0, 0, imgWidth, imgHeight, InputData, imgWidth, imgHeight, GDT_UInt16, 0, 0);
-//
-//        for (int i = 0; i < imgHeight; i++)    //循环图像高
-//        {
-//            //循环，将16bit数据专为8bit数据，使用线性拉伸方式
-//            for (int j = 0; j < imgWidth; j++)
-//            {
-//                if (InputData[i * imgWidth + j] < minpix)
-//                    OutputData[i * imgWidth + j] = 0;
-//                else if (InputData[i * imgWidth + j] >= minpix && InputData[i * imgWidth + j] < maxpix)
-//                {
-//                    double temp = ((double) InputData[i * imgWidth + j] - minpix) / stretched_gap * 255;
-//                    OutputData[i * imgWidth + j] = (GByte) temp;
-//                    //cout<<OutputData[i * imgWidth + j]<<endl;
-//                }
-//                if (InputData[i * imgWidth + j] > maxpix)
-//                    OutputData[i * imgWidth + j] = 255;
-//            }
-//        }
-//        OutputBand->RasterIO(GF_Write, 0, 0, imgWidth, imgHeight, OutputData, imgWidth, imgHeight, GDT_Byte, 0, 0);
-//    }
-//
-//    delete[] InputData;
-//    delete[] OutputData;
-//    //关闭原始图像和结果图像
-//    GDALClose((GDALDatasetH) OutputImage);
-//    GDALClose((GDALDatasetH) InputImage);
-//}
 
 Histogram::Histogram(QWidget *parent)
 {
